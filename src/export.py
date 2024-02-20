643681ead988cfec87a2532d91ab746b0407250c
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-02-20 06:41:12 +0100

    Add .gitignore and initial code files

---
+++ src/export.py
@@ -0,0 +1,17 @@
+from extract import extract
+#from transform import transform
+#from load import load
+
+def __main__():
+   print("STARTING")
+   print("EXTRACTING")
+   extractData = extract()
+   
+   print("TRANSFORMING")
+
+   print("LOADING")
+
+   print("DONE")
+
+if __name__== "__main__":
+    __main__()
---
+++ src/extract.py
@@ -0,0 +1,26 @@
+from os import getenv
+from dotenv import find_dotenv, load_dotenv
+from akeneo.akeneo import Akeneo
+import requests
+load_dotenv(find_dotenv())
+
+AKENEO_HOST = getenv('AKENEO_HOST')
+AKENEO_CLIENT_ID = getenv('AKENEO_CLIENT_ID')
+AKENEO_CLIENT_SECRET = getenv('AKENEO_CLIENT_SECRET')
+AKENEO_USERNAME = getenv('AKENEO_USERNAME')
+AKENEO_PASSWORD = getenv('AKENEO_PASSWORD')
+
+def getAkeneoProducts():
+  akeneo = Akeneo(
+    AKENEO_HOST,
+    AKENEO_CLIENT_ID,
+    AKENEO_CLIENT_SECRET,
+    AKENEO_USERNAME,
+    AKENEO_PASSWORD
+  )
+  searchQuery = '{"enabled":[{"operator":"=","value":true}],"completeness":[{"operator":"=","value":100}],"outdooractive_poi_category":[{"operator":"NOT EMPTY"}]}&search_scope=ecommerce'
+  return akeneo.getProducts(limit=100, search=searchQuery )
+
+def extract():
+  data = getAkeneoProducts()
+  return data
