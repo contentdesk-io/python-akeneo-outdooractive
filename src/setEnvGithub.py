bb7222f09ad3d07595d37c3c35b2ce02bfd77753
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-02-21 09:32:02 +0100

    Add secrets to environments and remove secret

--- src/setEnvGithub.py
+++ src/setEnvGithub.py
@@ -3,6 +3,7 @@ import json
 from base64 import b64encode
 from nacl import encoding, public
 from os import getenv
+import os
 from dotenv import find_dotenv, load_dotenv
 import glob
 
@@ -46,16 +47,48 @@ def createEncryptSecret(owner, repo, token, environment, secretName, secretValue
             'key_id': publicKey['key_id'],
         }))
     print(f'Added secret to {environment}: {encrypted_secret.status_code}')
-    print(encrypted_secret.text)
     print(f'Secret {secretName} added to {owner}/{repo}')
 
-def loadDotEnv():
+def removeSecret(owner, repo, token, environment, secretName):
+    headers = setHeaders(token)
+    response = requests.delete(f'https://api.github.com/repos/{owner}/{repo}/environments/{environment}/secrets/{secretName}', headers=headers)
+    print(f'Removed secret {secretName} from {environment}: {response.status_code}')
+    print(response.text)
+
+def getEnvironment():
     # Find all .env.xxx files
-    env_files = glob.glob('.env.*')
+    env_files = glob.glob('env/.env.*')
+    env_suffixes = [os.path.splitext(file)[1][1:] for file in env_files]
 
     # Print the list of files
-    print(env_files)
+    print(env_suffixes)
+    return env_suffixes
     
+def getSecrets(environment):
+    # Load the .env file
+    print(f'ENV - Loading .env.{environment}')
+    dotenvPath = 'env/.env.' + environment
+    # Secret to add
+    load_dotenv(dotenvPath, override=True)
+    # Set the environment variables
+    env_vars = {}
+    env_vars['AKENEO_HOST'] = getenv('AKENEO_HOST')
+    env_vars['AKENEO_CLIENT_ID'] = getenv('AKENEO_CLIENT_ID')
+    env_vars['AKENEO_CLIENT_SECRET'] = getenv('AKENEO_CLIENT_SECRET')
+    env_vars['AKENEO_USERNAME'] = getenv('AKENEO_USERNAME')
+    env_vars['AKENEO_PASSWORD'] = getenv('AKENEO_PASSWORD')
+    env_vars['CDN_ENDPOINT'] = getenv('CDN_ENDPOINT')
+    env_vars['OUTDOORACTIVE_KEY'] = getenv('OUTDOORACTIVE_KEY')
+    env_vars['OUTDOORACTIVE_SOURCE'] = getenv('OUTDOORACTIVE_SOURCE')
+    env_vars['OUTDOORACTIVE_OWNER'] = getenv('OUTDOORACTIVE_OWNER')
+    env_vars['OUTDOORACTIVE_OWNERNAME'] = getenv('OUTDOORACTIVE_OWNERNAME')
+    env_vars['S3_ENDPOINT'] = getenv('S3_ENDPOINT')
+    env_vars['S3_BUCKET'] = getenv('S3_BUCKET')
+    env_vars['S3_REGION'] = getenv('S3_REGION')
+    env_vars['S3_ACCESS_KEY'] = getenv('S3_ACCESS_KEY')
+    env_vars['S3_SECRET_ACCESS_KEY'] = getenv('S3_SECRET_ACCESS_KEY')
+    env_vars['S3_EXPORT_PATH'] = getenv('S3_EXPORT_PATH')
+    return env_vars
 
 def main():
     # Personal Access Token
@@ -72,28 +105,30 @@ def main():
     }
 
     # Environments to create
-    environments = ['test1', 'test2', 'test3']
+    #environments = ['test1', 'test2', 'test3']
+    environments = getEnvironment()
 
     # Create environments
     # https://docs.github.com/de/rest/deployments/environments?apiVersion=2022-11-28#create-or-update-an-environment
+    print(f'ENV - Creating environments for {owner}/{repo}')
     for env in environments:
         createEnvironment(owner, repo, token, env)
 
-    # Secret to add
-    secret_name = 'MY_SECRET'
-    secret_value = 'secret_value_here'
-
     # Add secret to environments
     # https://docs.github.com/de/rest/guides/encrypting-secrets-for-the-rest-api?apiVersion=2022-11-28#example-encrypting-a-secret-using-python
+    print(f'SECRETS - Adding secrets to {owner}/{repo}')
     for env in environments:
         publicKeyEnv = getPublicKey(owner, repo, token, env)
-        print(f'Public key: {publicKeyEnv}')
+        #print(f'Public key: {publicKeyEnv}')
 
-        secretValue = encrypt(publicKeyEnv['key'], secret_value)
-
-        print(f'Adding secret to {env}')
-        createEncryptSecret(owner, repo, token, env, secret_name, secretValue, publicKeyEnv)
+        # Get secrets from .env file
+        print(f'ENV - Getting secrets from .env.{env}')
+        secrets = getSecrets(env)
+        for key, value in secrets.items():
+            print(f'{key}: {value}')
+            secretValue = encrypt(publicKeyEnv['key'], value)
+            print(f'Adding secret {key} to {env}')
+            createEncryptSecret(owner, repo, token, env, key, secretValue, publicKeyEnv)
 
 if __name__ == '__main__':
-    loadDotEnv()
-    #main()
+    main()
